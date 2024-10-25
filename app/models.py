9b7bc9e94aaf196a9c1a6330de164c6d2a55d5b4
ademkoca <aademkocaa@gmail.com>
2024-10-25 16:24:25 +0200

    add comment

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
-
+##commented out for backwards compatibility
 class Product(Base):
     __tablename__ = "products"
     
